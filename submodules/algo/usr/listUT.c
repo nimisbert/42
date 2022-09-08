596840a08b7e3135ea75eb27ecb958a64526bbee
nimisbert <n.misbert@yahoo.fr>
2022-09-08 21:28:58 +0200

    some changes

--- submodules/algo/usr/listUT.c
+++ submodules/algo/usr/listUT.c
@@ -7,12 +7,19 @@ void main(void)
     //
     List L;
     list_init(&L,(void*)free);
+
     list_innext(&L, NULL, (const void *)5); 
+
     Node *h = list_head(&L);
+
     list_innext(&L, h, (const void*)6);
+
     Node *t = list_tail(&L);
+
     void *removedData;
+
     list_rmnext(&L, h, &removedData);
+    
     list_clean(&L);
 
     // --- double linked list UT --- //
