98a800ed8b41458eab51bae24aa2c07e147a0b66
nimisbert <n.misbert@yahoo.fr>
2022-09-08 21:38:24 +0200

    some modifs for conflicts

--- submodules/algo/usr/listUT.c
+++ submodules/algo/usr/listUT.c
@@ -11,25 +11,24 @@ void main(void)
     dlist_innext(&DL, NULL, (const void *)5);
     Dnode *dh = list_head(&DL);
 
-    // --- linked list UT --- //
+    // --- Double linked list UT --- //
     //
-    
-    List L;
-    list_init(&L,(void*)free);
-
-    list_innext(&L, NULL, (const void *)5); 
-
-    Node *h = list_head(&L);
-
-    list_innext(&L, h, (const void*)6);
-
-    Node *t = list_tail(&L);
-
-    void *removedData;
-
-    list_rmnext(&L, h, &removedData);
-    
-    list_clean(&L);
+    Dlist DL;
+    dlist_init(&DL,(void*)free);
+    dlist_innext(&DL, NULL, (const void *)5);
+    Dnode *dh = list_head(&DL);
+// --- Double linked list UT --- //
+    //
+    Dlist DL;
+    dlist_init(&DL,(void*)free);
+    dlist_innext(&DL, NULL, (const void *)5);
+    Dnode *dh = list_head(&DL);
+// --- Double linked list UT --- //
+    //
+    Dlist DL;
+    dlist_init(&DL,(void*)free);
+    dlist_innext(&DL, NULL, (const void *)5);
+    Dnode *dh = list_head(&DL);
 
    
 
