b438e573c656e2d301b570870b2aeb42bfcac48e
nm <n.misbert@yahoo.fr>
2023-03-19 13:35:44 +0100

    fixed problem statement

--- SW/examples/main_set.c
+++ SW/examples/main_set.c
@@ -13,28 +13,94 @@ typedef struct KSet_ {
 } KSet;
 
 // KSet, added layer... so weird
-int kset_init        ( KSet *Kens, uint8_t Kkey);   // ToTest
-int kset_destroy     ( KSet *Kens);                 // ToDo
+int kset_init        ( KSet *Kens, uint8_t Kkey); 
+int kset_destroy     ( KSet *Kens);                
+int set_kset_insert  ( Set *ens, KSet *kens);
 
+// Set of char 
 int set_char_insert  ( Set *ens, char  c);
-int set_char_print   ( Set *ens);
 int set_char_corresp ( const void *cle1, const void *cle2);
+
+// Cover problem
+int set_problem_print( Set *S, Set *P);
 int set_cover        ( Set *elements, Set *sous_ens, Set *couverture); 
 
 void main(void)
 {
+// Init
     Set S, C, P;
     KSet A[7];
-    int8_t ret;
+    int8_t ret = 0;
     set_init( &S, set_char_corresp, free);
     set_init( &C, set_char_corresp, free);
     set_init( &P, set_char_corresp, free);
     for( int i = 0; i < 7; i++) {
         ret = kset_init( &A[i], i);
     }
+    for( int i = 0; i < 7; i++) {
+        ret = set_kset_insert( &P, &A[i]);
+        printf("Insert A%d in P ret %d\r\n",i,ret);
+    }
+// State exemple problem page 133
+    for( char iter = 'a'; iter < 'm'; iter++) {
+        set_char_insert( &S, iter);
+        switch( iter ) {
+            case 'a':
+                set_char_insert( &A[0].ens, iter);
+                set_char_insert( &A[3].ens, iter);
+                break;
+            case 'b':
+                set_char_insert( &A[0].ens, iter);
+                set_char_insert( &A[4].ens, iter);
+                break;
+            case 'c':
+                set_char_insert( &A[0].ens, iter);
+                set_char_insert( &A[5].ens, iter);
+                break;
+            case 'd':
+                set_char_insert( &A[0].ens, iter);
+                set_char_insert( &A[5].ens, iter);
+                break;
+            case 'e':
+                set_char_insert( &A[1].ens, iter);
+                set_char_insert( &A[3].ens, iter);
+                break;
+            case 'f':
+                set_char_insert( &A[1].ens, iter);
+                set_char_insert( &A[4].ens, iter);
+                break;
+            case 'g':
+                set_char_insert( &A[1].ens, iter);
+                set_char_insert( &A[4].ens, iter);
+                set_char_insert( &A[5].ens, iter);
+                break;
+            case 'h':
+                set_char_insert( &A[1].ens, iter);
+                set_char_insert( &A[5].ens, iter);
+                break;
+            case 'i':
+                set_char_insert( &A[1].ens, iter);
+                break;
+            case 'j':
+                set_char_insert( &A[2].ens, iter);
+                break;
+            case 'k':
+                set_char_insert( &A[2].ens, iter);
+                set_char_insert( &A[5].ens, iter);
+                break;
+            case 'l':
+                set_char_insert( &A[2].ens, iter);
+                set_char_insert( &A[5].ens, iter);
+                set_char_insert( &A[6].ens, iter);
+                break;
+            default:
+                break;
+        }
+    }
+// Display problem
+    set_problem_print( &S, &P);
 
-    
-
+// Clean
     for( int i = 0; i < 7; i++) {
         kset_destroy( &A[i]);
     }
@@ -59,7 +125,16 @@ int kset_init( KSet *Kens, uint8_t Kkey)
 int kset_destroy( KSet *Kens)
 {
     free( Kens->cle );
-    set_destroy( &Kens->ens );
+    set_destroy( &Kens->ens );   
+    return 0;
+}
+
+int set_kset_insert( Set *ens, KSet *kens)
+{
+    if( set_insert( ens, kens) != 0) {
+        return -1;
+    }
+    return 0;
 }
 
 int set_char_insert( Set *ens, char c)
@@ -75,20 +150,37 @@ int set_char_insert( Set *ens, char c)
     return 0;
 }
 
-int set_char_print( Set *ens)
+int set_problem_print( Set *S, Set *P)
 {
-    ListElmt *elt;
-    char *donnee;
+    ListElmt *elt_iter, *kset_iter;
+    char *set_donnee;
+    uint8_t *kset_donnee;
+
+    printf("S (Set of elements) : \r\n");
     printf("{ ");
-    for( elt = list_head(ens); elt != NULL; elt = list_next(elt)) {
-        printf("%s",(char*)list_data(elt));
-        if( elt->suivant != NULL ) {
+    for( elt_iter = list_head(S); elt_iter != NULL; elt_iter = list_next(elt_iter)) {
+        printf("%s",(char*)list_data(elt_iter));
+        if( elt_iter->suivant != NULL ) {
             printf(", ");
         } else {
             printf(" ");
         }
     }
     printf("}\r\n");
+    printf("P (Set of sets of elements) : \r\n");
+    for( kset_iter = list_head(P); kset_iter != NULL; kset_iter = list_next(kset_iter)) {
+        kset_donnee = (uint8_t*)((KSet*)kset_iter->donnee)->cle;
+        printf("Subset A%d : { ", *kset_donnee);
+        for( elt_iter = list_head(&(((KSet*)kset_iter->donnee)->ens)); elt_iter != NULL; elt_iter = list_next( (elt_iter)) ) {
+            printf("%s",(char*)list_data(elt_iter));
+            if( elt_iter->suivant != NULL ) {
+                printf(", ");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("}\r\n");
+    }
     return 0;
 }
 
