c2e7a841fd756aa891407439ed1281162986ebe6
nm <n.misbert@yahoo.fr>
2023-05-14 20:12:31 +0200

    graph with Haskell-Chart

--- SW/hmodel/app/Main.hs
+++ SW/hmodel/app/Main.hs
@@ -1,5 +1,24 @@
 module Main (main) where
+-- Graphics Library
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Diagrams(toFile)
+-- Own Library
+import Models.LotkaVolterra
 
+deers :: Population
+deers = VerhulstParameters 10 30 0.2
 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+time :: [Float]
+time  = [0.0, 1.0 .. 100.0]
+
+populationCount :: [Float]
+populationCount = map (logisticEquation deers) time
+
+timeSerie = zip time populationCount
+
+main :: IO()
+main = toFile def "Logistic Equation.png" $ do
+    layout_title .= "Deer population model"
+    setColors [opaque blue, opaque red]
+    plot (line "Deers" [timeSerie])
+    plot (points "Data Points" (timeSerie))
--- SW/hmodel/src/Models/LotkaVolterra.hs
+++ SW/hmodel/src/Models/LotkaVolterra.hs
@@ -1,3 +1,7 @@
+module Models.LotkaVolterra (
+    Population(..)
+  , logisticEquation
+) where 
 
 data Population = VerhulstParameters {
     headCount        :: Float -- N, P
@@ -5,10 +9,6 @@ data Population = VerhulstParameters {
   , growthRate       :: Float -- r
 } deriving (Show)
 
-logisticEquation :: Population -> Population
-logisticEquation (VerhulstParameters p k r) = 
-  (VerhulstParameters (p + r*p*(1-(p/k))) k r)
-
-lotkaVolterraEquation :: Population -> Population -> Float -> Population
-lotkaVolterraEquation (VerhulstParameters p1 k1 r1) (VerhulstParameters p2 k2 r2) alpha =
-  (VerhulstParameters (p1 + r1*p1*(k1-p1-alpha*p2)/k1)) k1 r1
+logisticEquation :: Population -> Float -> Float
+logisticEquation (VerhulstParameters p0 k r) t = 
+  k / (1 + ((k-p0) / p0) * exp( - r * t ))
